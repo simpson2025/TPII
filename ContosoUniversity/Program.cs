63e473313ec558985f9140f29ef94783d02e5ed4
constantin <25constantino1985@gmail.com>
2026-07-07 15:51:55 -0400

    BDcreation

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -1,9 +1,18 @@
+using ContosoUniversity.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+builder.Services.AddDbContext<SchoolContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
