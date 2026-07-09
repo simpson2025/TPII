185e1ee7e5b6e9d31b0215755da96894e356eecd
constantin <25constantino1985@gmail.com>
2026-07-08 21:11:44 -0400

    Donnees_Associees

--- ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/Models/Course.cs
@@ -1,15 +1,29 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
     public class Course
     {
+
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Number")]
         public int CourseID { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
         public string Title { get; set; }
+
+        [Required]
+        [Range(0, 5)]
         public int Credits { get; set; }
 
-        public ICollection<Enrollment> Enrollments { get; set; }
+        public int DepartmentID { get; set; }
+
+        public Department? Department { get; set; }
+        public ICollection<Enrollment>? Enrollments { get; set; }
+        public ICollection<CourseAssignment>? CourseAssignments { get; set; }
     }
 }
